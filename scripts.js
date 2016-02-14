89b882b8c9ce71eefc66741a8dd514069d2b3aff
Nerdenator <a.mcruer@live.com>
2016-02-14 12:21:39 -0600

    added jquery, updated scripts

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 
 var script = document.createElement('script');
-script.src = "https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"
+script.src = "https://code.jquery.com/ui/1.11.4/jquery-ui.min.js";
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
